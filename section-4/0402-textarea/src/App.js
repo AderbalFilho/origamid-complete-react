4a04446a5f4e08374ebf6658e1b80c54c25fc451
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-03-09 09:59:50 -0300

    put textarea surrounded by form tag

--- section-4/0402-textarea/src/App.js
+++ section-4/0402-textarea/src/App.js
@@ -3,11 +3,13 @@ import React, { useState } from 'react';
 const App = () => {
   const [textarea, setTextarea] = useState('');
   return (
-    <textarea
-      value={textarea}
-      onChange={({ target }) => setTextarea(target.value)}
-      rows="5"
-    />
+    <form>
+      <textarea
+        value={textarea}
+        onChange={({ target }) => setTextarea(target.value)}
+        rows="5"
+      />
+    </form>
   );
 };
 

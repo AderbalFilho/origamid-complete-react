b07f2aa5e54bb4bf02ca15c22fab1f2f965f0a07
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-03-09 13:37:42 -0300

    section 4 lesson 0408

--- section-4/0406-0408-input-select-component/src/App.js
+++ section-4/0406-0408-input-select-component/src/App.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 
 import Input from './Form/Input';
+import Radio from './Form/Radio';
 import Select from './Form/Select';
 
 const App = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [product, setProduct] = useState('');
+  const [color, setColor] = useState('');
+  const [fruits, setFruits] = useState('');
 
   return (
     <form>
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Vermelho']} value={color} setValue={setColor} />
+      <h2>Frutas</h2>
+      <Radio
+        options={['Limão', 'Laranja', 'Uva']}
+        value={fruits}
+        setValue={setFruits}
+      />
+
       <Select
         options={['Smartphone', 'Tablet']}
         value={product}

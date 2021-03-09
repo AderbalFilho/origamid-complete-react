5720f5a5fde760fe340b48b38d383f396fd3baf5
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-03-09 13:11:18 -0300

    section 4 lesson 0407

--- section-4/0406-0407-input-select-component/src/App.js
+++ section-4/0406-0407-input-select-component/src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 
 import Input from './Form/Input';
+import Select from './Form/Select';
 
 const App = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [product, setProduct] = useState('');
 
   return (
     <form>
+      <Select
+        options={['Smartphone', 'Tablet']}
+        value={product}
+        setValue={setProduct}
+      />
       <Input id="nome" label="Nome" value={name} setValue={setName} required />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
       <button>Enviar</button>

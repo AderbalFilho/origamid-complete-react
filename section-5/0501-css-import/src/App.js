a5904ccefae4be78df90fd927cc42c0999a5cdc0
AderbalFilho <aderbalcrato@yahoo.com.br>
2021-03-12 15:15:11 -0300

    section 5 lesson 0501

---
+++ section-5/0501-css-import/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Title from './Title';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div class="container">
+      <Title />
+    </div>
+  );
+};
+
+export default App;
